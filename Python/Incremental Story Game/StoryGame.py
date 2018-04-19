b038ea91f9867aa21348340a55507a4a092e4b74
nqdoan <nqdoan2@uh.edu>
2018-04-18 21:56:55 -0500

    Added IncrementalStoryProject
    Added IncrementalStoryProject

--- Python/Incremental Story Game/StoryGame.py
+++ Python/Incremental Story Game/StoryGame.py
@@ -1,4 +1,8 @@
-def main():
+import csv
+import random
+
+def gameView():
+    
     print ("****** Text Adventure Game v1.0 ******")
     print ("*                                    *")
     print ("*          1 - New Game              *")
@@ -6,9 +10,80 @@ def main():
     print ("*          3 - Quit                  *")
     print ("*                                    *")
     print ("**************************************")
+
+def runGame(startValue, storyData, rowCount):
     
-    userInput = int(input("What do you want to do?"))
+    for i in range(startValue, rowCount, 1):
+        
+        #Checks to see if it's the end, then chooses a random ending from what's left over in the file.
+        if not(storyData[i][1]):
+            randomEnding = random.randint(i, rowCount)
+            print (storyData[randomEnding][0])
+            break
+    
+        #Prints Options if they are included
+        print (storyData[i][0])
+        print ("1 - " + storyData[i][1])
+        print ("2 - " + storyData[i][2])
+        print ("3 - Save Game")
+        
+        #Alternative way to do above
+        '''
+            for j in range(1,3,1):
+            print(str(j) + " - " + storyData[i][j])
+            print ("3 - Save Game")
+            '''
+        
+        
+        
+        ui = userInput()
+        if(ui == 3):
+            print (">>> Game Saved!")
+            return i
+            break
 
 
+def userInput():
+    #Gathers user input
+    while True:
+        try:
+            userInput = int(input("What do you want to do?\n"))
+            break
+        except(ValueError):
+            print("Error with input. Please try again!")
+    return userInput
 
-main() 
+
+def main():
+    storyData = []
+    rowCount = 0
+    #store data in storyData array
+    infile = open("story.csv", "r")
+    reader = csv.reader(infile)
+    for row in reader:
+        storyData.append(row)
+        rowCount = rowCount + 1
+    print (rowCount)
+
+
+#initialize run values
+game = 0
+    savedStartValue = 0
+    while(game == 0):
+        #Start running the game, call functions gameView and userInput
+        gameView()
+        initialInput = userInput()
+        
+        #Loop for view options 1, 2 and 3
+        if(initialInput == 1):
+            savedStartValue = 0
+            rg = runGame(savedStartValue, storyData, rowCount)
+            savedStartValue = rg
+        elif(initialInput == 2):
+            loadGame = runGame(savedStartValue, storyData, rowCount)
+        elif(initialInput == 3):
+            print ("Quitting game!")
+
+
+infile.close()
+main()
